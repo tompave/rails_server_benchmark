f08b7cf78d2cd1fcaaf08c8fff6ff5e9fa754815
tom <tommaso@pavese.me>
2016-12-08 00:57:05 +0000

    removed the old benchmark script

--- script/benchmark.rb
+++ script/benchmark.rb
@@ -3,12 +3,10 @@
 # ---------------------------------------------------------
 # Configuration
 
-concurrency_level = 50
-seconds = 30
+seconds = 5
 # total_request = 400
+CONCURRENCY_LEVELS = [1, 10, 20, 30, 40, 50]
 
-tool = :ab
-# tool = :siege
 
 if !defined?(seconds) && !defined?(total_request)
   exit "You must either define the duration of the test or the number of requests"
@@ -27,55 +25,45 @@ end
 #   stop after 30 seconds:
 #     ab -c 30 -t 30
 #
-# Siege:
-#   stop after 400 requests:
-#     siege -b -c 30 -r 400
-#   stop after 30 seconds:
-#     siege -b -c 30 -t 30s
-#
 
-if tool == :ab
-  base_cmd = "ab -r -c #{concurrency_level}"
+@base_cmd = "ab -r -c %{concurrency_level}"
 
-  if defined?(seconds) && seconds
-    base_cmd << " -t #{seconds}"
-  elsif defined?(total_request) && total_request
-    base_cmd << " -n #{total_request}"
-  end
-elsif tool == :siege
-  base_cmd = "siege -b -c #{concurrency_level}"
-
-  if defined?(seconds) && seconds
-    base_cmd << " -t #{seconds}s"
-  elsif defined?(total_request) && total_request
-    base_cmd << " -r #{total_request}"
-  end
+if defined?(seconds) && seconds
+  @base_cmd << " -t #{seconds}"
+elsif defined?(total_request) && total_request
+  @base_cmd << " -n #{total_request}"
 end
 
+@base_cmd << " %{url}"
 
 # ---------------------------------------------------------
+def blank?(str)
+  str.nil? || str.strip.empty?
+end
+# ---------------------------------------------------------
 
-endpoints = [
-  "/fibonacci/:number", # 0.34 seconds on average on my machine
+ENDPOINTS = [
+  "/fibonacci/32", # 0.34 seconds on average on my machine
   "/template-render",
   "/template-render-no-response",
   "/network-io",
   "/network-io-and-render",
-  "/pause/:seconds",
-  "/pause-and-render/:seconds"
+  "/pause/2",
+  "/pause-and-render/2"
 ]
 
 
 
-@base_url = "http://127.0.0.1:3000"
+BASE_URL = "http://127.0.0.1:3000"
 
 def url_for(path)
-  URI.join(@base_url, path).to_s
+  URI.join(BASE_URL, path).to_s
 end
 
 # ---------------------------------------------------------
 
 require 'uri'
+require 'csv'
 
 output_file_path = File.expand_path("../../bench_results.txt", __FILE__)
 @out_file = File.open(output_file_path, "w")
@@ -85,23 +73,61 @@ def log(str)
   @out_file.puts "\n"
 end
 
+def write_row(target, type, results)
+  str = CSV.generate_line([target, type, *results])
+  @out_file.puts str
+end
 # ---------------------------------------------------------
 
-log Time.now.to_s
 
-endpoints.each do |path|
-  log "====================================" * 3
 
-  cmd = "#{base_cmd} #{url_for(path)}"
 
-  # suppress the request logging for Siege
-  cmd << " 1> /dev/null" if tool == :siege
+# ---------------------------------------------------------
 
-  puts cmd
-  log ">>>  " + cmd
-
-  output = `#{cmd}`
-  log output
+REQ_P_S_REGEX = %r{Requests per second\:\s+(\d+\.\d*) \[\#\/sec\] \(mean\)}
+T_P_REQ_REGEX = %r{Time per request\:\s+(\d+\.\d*) \[ms\] \(mean\)}
+def extract_data(str)
+  return nil if blank?(str)
+  req_p_s = REQ_P_S_REGEX.match(str)&.[](1).to_s
+  t_p_req = T_P_REQ_REGEX.match(str)&.[](1).to_s
+  [req_p_s, t_p_req]
 end
+
+# ---------------------------------------------------------
+
+
+@results = {
+  req_p_sec: {},
+  resp_time: {}
+}
+
+def run_test(conc_requests, target)
+  cmd = sprintf(@base_cmd, concurrency_level: conc_requests, url: target)
+  print ">>> #{cmd}"
+  output = `#{cmd}`
+end
+
+
+write_row("path", "metric", CONCURRENCY_LEVELS)
+
+ENDPOINTS.each do |path|
+  target = url_for(path)
+  req_p_s_results = []
+  resp_time_results = []
+
+  CONCURRENCY_LEVELS.each do |conc_req|
+    results = run_test(conc_req, target)
+    req_p_s, resp_time = extract_data(results)
+
+    puts "  req/s: #{req_p_s}, resp time: #{resp_time}"
+
+    req_p_s_results << req_p_s
+    resp_time_results << resp_time
+  end
+
+  write_row(path, "req/s", req_p_s_results)
+  write_row(path, "resp_time", resp_time_results)
+end
+
 
 @out_file.close
